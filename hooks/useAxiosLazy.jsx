15c2db70124d3cb69493411f83e1b7d8c590034a
Kevin Addin <addinkevin@gmail.com>
2020-11-26 07:52:16 -0300

    feat: useAxiosLazy hook

---
+++ hooks/useAxiosLazy.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import axios from "axios";
+
+const useAxiosLazy = () => {
+  const [data, setData] = React.useState();
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState();
+  const [called, setCalled] = React.useState(false);
+
+  const dispatch = React.useCallback((url) => {
+    const fetchData = async () => {
+      try {
+        const r = await axios.get(url);
+        setData(r.data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+        setError(error);
+      }
+    };
+    setLoading(true);
+    setCalled(true);
+    fetchData();
+  });
+
+  return [dispatch, { data, loading, error, called }];
+};
+
+export default useAxiosLazy;
